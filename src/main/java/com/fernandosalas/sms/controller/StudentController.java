6117f46d49700769224889ee36f79a4aa0974d3c
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-21 06:17:33 -0500

    Create Student Form Validation

--- src/main/java/com/fernandosalas/sms/controller/StudentController.java
+++ src/main/java/com/fernandosalas/sms/controller/StudentController.java
@@ -2,10 +2,12 @@ package com.fernandosalas.sms.controller;
 
 import com.fernandosalas.sms.dto.StudentDto;
 import com.fernandosalas.sms.service.StudentService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,7 +40,13 @@ public class StudentController {
 
     //handle method to handle save student form submit request
     @PostMapping("/students")
-    public String saveStudent(@ModelAttribute("student") StudentDto studentDto) {
+    public String saveStudent(@Valid @ModelAttribute("student") StudentDto studentDto,
+                              BindingResult result,
+                              Model model) {
+        if(result.hasErrors()) {
+            model.addAttribute("student", studentDto);
+            return "create_student";
+        }
         studentService.createStudent(studentDto);
         return "redirect:/students";
     }
--- src/main/java/com/fernandosalas/sms/dto/StudentDto.java
+++ src/main/java/com/fernandosalas/sms/dto/StudentDto.java
@@ -1,4 +1,6 @@
 package com.fernandosalas.sms.dto;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,14 @@ import lombok.Setter;
 @AllArgsConstructor
 public class StudentDto {
     private Long id;
+
+    @NotEmpty(message = "Student first name should not be empty")
     private String firstName;
+
+    @NotEmpty(message = "Student last name should not be empty")
     private String lastName;
+
+    @NotEmpty(message = "Student email should not be empty")
+    @Email
     private String email;
 }
